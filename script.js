ea98542126b26d10f5a991ef098dbed533921b24
Lizzy-j <lizzyhj@hotmail.com>
2020-05-13 00:33:10 +0100

    fix minor bug in navbar

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-toggleMenu{
+function toggleMenu(){
     document.querySelector(".navigation_menu--icon").classList.toggle("display")
 };
 
 
 window.onclick = function(event) {
     if (!event.target.matches('.navigation_menu--icon')) {
-        let dropdowns = document.getElementsByClassName("nav_link-container");
-        let i;
-        for (i = 0; i < dropdowns.length; i++) {
+        let dropdowns = document.querySelector(".nav_link-container");
+        for (let i = 0; i < dropdowns.length; i++) {
           let openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('let')) {
-            openDropdown.classList.remove('let');
+          if (openDropdown.classList.contains('display')) {
+            openDropdown.classList.remove('display');
           }
         }
       }
