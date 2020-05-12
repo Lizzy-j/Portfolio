75a12815e3497626c64935e2bcb8745e8c437a5b
Lizzy-j <lizzyhj@hotmail.com>
2020-05-12 23:53:08 +0100

    add functionality to toggle menu

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-toggleMenu{}
+toggleMenu{
+    document.querySelector(".navigation_menu--icon").classList.toggle("display")
+};
+
+
+window.onclick = function(event) {
+    if (!event.target.matches('.navigation_menu--icon')) {
+        let dropdowns = document.getElementsByClassName("nav_link-container");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+          let openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('let')) {
+            openDropdown.classList.remove('let');
+          }
+        }
+      }
+}
